6d4c7090a96ca04e9951de54b6c102b37663dbc6
kenken0413 <hokkaider-0413@outlook.jp>
2019-10-30 22:02:51 +0900

    change index

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
   before_action :set_group,only: [:edit, :update]
   def index
-    @groups = Group.find(params[:id])
+    # @groups = Group.find(params[:id])
   end
 
   def new
